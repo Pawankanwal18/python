782fc5711b62a8ff4f56e762f14940eb19cba026
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-06-12 00:29:46 +0530

    added

--- import.py
+++ import.py
@@ -3,6 +3,14 @@
 #result = math.sqrt(9)
 #rint(result)
 
-from math import sqrt, pi
-result =sqrt(9)*pi
-print(result)
+#from math import sqrt, pi
+#result =sqrt(9)*pi
+#print(result)
+
+from pawan import welcome, pawan
+import math
+
+print(dir(math))
+print(math.nan, type(math.nan))
+welcome()
+print(pawan)
